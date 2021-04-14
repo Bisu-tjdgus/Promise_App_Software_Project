4b461489ac29740b38d17edd8922c7d44026c44f
Bisu-tjdgus <tjdgus726@naver.com>
2021-04-14 16:47:43 +0900

    초안

---
+++ PromiseApp/app/src/main/java/com/example/promiseapp/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.example.promiseapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_camera);
+    }
+}
