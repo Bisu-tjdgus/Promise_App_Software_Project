5454ba47574d8e5cecf53e94dc80d676618d0b73
조성현 <50747571+Bisu-tjdgus@users.noreply.github.com>
2021-05-17 23:54:16 +0900

    Add files via upload

---
+++ src/main/java/com/example/promiseapp/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.promiseapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchscreen);
+    }
+}
